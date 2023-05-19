8665a6e405cf8ffa92ab81fcd0af82c89497d9b4
AndriyNich <info@bembi.ua>
2023-05-20 02:08:05 +0300

    card

--- src/components/TweetsList/TweetInfo/TweetInfo.jsx
+++ src/components/TweetsList/TweetInfo/TweetInfo.jsx
@@ -1,9 +1,20 @@
-import { Avatar, Card } from './TweetInfo.styled';
+import { Avatar, Button, Card, Followers, Tweets } from './TweetInfo.styled';
 
 export const TweetInfo = ({ info }) => {
   return (
     <Card>
       <Avatar src={info.avatar} width="62" height="62" alt="avatar" />
+      <Tweets>{info.tweets} tweets</Tweets>
+      <Followers>
+        {new Intl.NumberFormat('en-US').format(info.followers)} followers
+      </Followers>
+      <Button
+        onClick={() => {
+          console.log('clicl');
+        }}
+      >
+        Follow
+      </Button>
     </Card>
   );
 };
--- src/components/TweetsList/TweetInfo/TweetInfo.styled.js
+++ src/components/TweetsList/TweetInfo/TweetInfo.styled.js
@@ -19,3 +19,49 @@ export const Avatar = styled.img`
   top: 187px;
   border-radius: 50%;
 `;
+
+export const Tweets = styled.p`
+  margin: 284px 0 0 0;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 1.2;
+  text-transform: uppercase;
+  text-align: center;
+
+  color: #ebd8ff;
+`;
+
+export const Followers = styled.p`
+  margin: 16px 0 0 0;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 1.2;
+  text-transform: uppercase;
+  text-align: center;
+  color: #ebd8ff;
+`;
+
+export const Button = styled.button`
+  display: block;
+  margin: 26px auto 0 auto;
+  width: 196px;
+  height: 50px;
+
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 1.22;
+
+  text-transform: uppercase;
+
+  color: #373737;
+  background: #ebd8ff;
+  box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  cursor: pointer;
+`;
